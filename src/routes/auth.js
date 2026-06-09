40b03e1e1c8cd6449434331e20a1258129d3de51
sandrosiqueiradavid <112038841+sandrosiqueiradavid@users.noreply.github.com>
2026-06-08 21:38:29 -0300

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/authController')
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', authController.register)
+router.post('/login', authController.login)
 
-module.exports = router;
+module.exports = router
